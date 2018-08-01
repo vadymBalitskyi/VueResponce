38725dde736171b2582a8081dc359aab883beada
Vad <vadim.balitskyi@gmail.com>
2018-08-01 17:41:39 +0300

    add project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Test/>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    background-color: #fbfbfb;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+import Vue from 'vue'
+import App from './App'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
